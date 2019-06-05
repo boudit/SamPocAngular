3a6a5beb9d0f467f77d78cf3dc0dac4b15476f02
boudit <J8OW@eurofins.com>
2019-06-05 15:08:59 +0200

    InMemoryDataService

--- src/app/proxy/proxy.module.ts
+++ src/app/proxy/proxy.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule }    from '@angular/common/http';
+import { InMemoryDataService } from './service/in-memory-data.service';
+import { CustomerDto } from './dto/customer-dto';
 
 @NgModule({
-  declarations: [],
+  declarations: [InMemoryDataService],
   imports: [
     CommonModule,
     HttpClientModule
+  ],
+  exports: [
+    CustomerDto
   ]
 })
 export class ProxyModule { }
