0dfec7b02a5314b35d6077e4a05d32758e6edc18
boudit <J8OW@eurofins.com>
2019-06-07 11:55:17 +0200

    ThibaudModule

---
+++ src/app/thibaud/thibaud.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ComponentModule } from '../component/component.module';
+import { ActivityModule } from '../activity/activity.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ComponentModule,
+    ActivityModule
+  ]
+})
+export class ThibaudModule { }
